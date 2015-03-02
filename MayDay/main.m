2050b4d9055d594e0c0745b01d62750e0a9b0f3a
Jack Shultz <jackygrahamez@gmail.com>
2015-03-01 21:42:47 -0500

    Initial Commit

---
+++ MayDay/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MayDay
+//
+//  Created by John Shultz on 3/1/15.
+//  Copyright (c) 2015 John Shultz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

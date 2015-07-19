1b41992a3c254df86d2b531243c9d7d9165a4bd4
Jack Shultz <jackygrahamez@gmail.com>
2015-07-19 10:32:24 -0400

    added watchkit

---
+++ textsosalert WatchKit Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  textsosalert WatchKit Extension
+//
+//  Created by John Shultz on 7/19/15.
+//  Copyright (c) 2015 John Shultz. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
---
+++ textsosalert WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  textsosalert WatchKit Extension
+//
+//  Created by John Shultz on 7/19/15.
+//  Copyright (c) 2015 John Shultz. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end

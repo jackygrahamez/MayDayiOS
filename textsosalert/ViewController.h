e5bc686875bdf620d164106cd17d1920cb20b016
Jack Shultz <jackygrahamez@gmail.com>
2015-07-19 10:21:40 -0400

    Initial Commit

---
+++ textsosalert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  textsosalert
+//
+//  Created by John Shultz on 7/19/15.
+//  Copyright (c) 2015 John Shultz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
